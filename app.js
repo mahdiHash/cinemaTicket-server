b29a3fff73a9a35553e72c768e01dafa3eb61f4b
mahdiHash <mahdihashemiii939@gmail.com>
2023-02-04 17:44:40 +0330

    feat: add swagger documentaion UI

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDoc = require('./swagger.json');
 
 const app = express();
 
@@ -8,5 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
+
+// swagger docs
+app.use('/apidocs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 module.exports = app;
