ebf9cfa904fc97113b929102d7fbe29f77aa5351
mahdiHash <mahdihashemiii939@gmail.com>
2023-02-05 23:37:34 +0330

    feat: create a cipher for hashing

--- utils/cipherFunc.js
+++ utils/cipherFunc.js
@@ -1,21 +1,23 @@
 require('dotenv').config();
 const crypto = require('crypto');
-const cipher = crypto.createCipheriv(
-  process.env.CIPHER_ALGORITHM,
-  process.env.CIPHER_KEY,
-  process.env.CIPHER_IV
-);
-const decipher = crypto.createDecipheriv(
-  process.env.CIPHER_ALGORITHM,
-  process.env.CIPHER_KEY,
-  process.env.CIPHER_IV
-)
 
 function encrypt(input = "") {
+  const cipher = crypto.createCipheriv(
+    process.env.CIPHER_ALGORITHM,
+    process.env.CIPHER_KEY,
+    process.env.CIPHER_IV
+  );
+
   return cipher.update(input, 'utf-8', 'hex');
 }
 
 function decrypt(input = "") {
+  const decipher = crypto.createDecipheriv(
+    process.env.CIPHER_ALGORITHM,
+    process.env.CIPHER_KEY,
+    process.env.CIPHER_IV
+  );
+
   return decipher.update(input, 'hex', 'utf-8');
 }
 
