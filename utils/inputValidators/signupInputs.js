683fc53fe53f3e1e44ea1eaab02047339966d45f
mahdiHash <mahdihashemiii939@gmail.com>
2023-02-06 17:40:01 +0330

    chore: <signup> add error messsages to input validators

--- utils/inputValidators/signupInputs.js
+++ utils/inputValidators/signupInputs.js
@@ -2,13 +2,21 @@ const Joi = require('joi');
 const schema = Joi.object({
   tel: Joi.string()
     .trim()
-    .length(11),
+    .length(11)
+    .messages({
+      "string.length": "tel filed must contain 11 characters."
+    }),
 
   password: Joi.string()
     .trim()
-    .min(6),
+    .empty()
+    .min(6)
+    .messages({
+      "string.empty": "password can't be empty.",
+      "string.min": "password should contain at least 6 characters.",
+    }),
 
-  repeatPass: Joi.ref('password')
+  repeatPass: Joi.ref('password'),
 })
   .with('password', 'repeatPass');
 
