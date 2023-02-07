a48bc94ba8cae8516850e5943497170516d8b26a
mahdiHash <mahdihashemiii939@gmail.com>
2023-02-07 14:22:39 +0330

    fix(apikey): check for duplicate api key before sending it to clinet

--- controllers/others/getapikey.js
+++ controllers/others/getapikey.js
@@ -2,7 +2,16 @@ const crypto = require('crypto');
 const prisma = require('../../config/prismaConfig');
 
 const controller = async (req, res, next) => {
-  let key = crypto.randomBytes(32).toString('hex');
+  let key;
+  
+  while (true) {
+    key = crypto.randomBytes(32).toString('hex');
+    let duplicate = await prisma.api_keys.findFirst({ where: { key }});
+
+    if (!duplicate) {
+      break;
+    }
+  }
 
   await prisma.api_keys.create({
     data: {
