e4b4e47b136449d1f5bc0f9781d626e669e6ca65
mahdiHash <mahdihashemiii939@gmail.com>
2023-02-11 02:29:03 +0330

    feat(route): add user's profile update route

--- routers/user.js
+++ routers/user.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const getUserProfile = require('../controllers/user/getUserProfile');
+const updateProfile = require('../controllers/user/updateProfile');
 
 router.get('/profile', getUserProfile);
+
+router.put('/update', updateProfile);
 
 module.exports = router;
