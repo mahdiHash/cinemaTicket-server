f1630cca7401594920380fca64d94de6d4c35055
mahdiHash <mahdihashemiii939@gmail.com>
2023-02-11 14:10:14 +0330

    chore(validator): make fields required on signup and login

--- utils/inputValidators/loginInputs.js
+++ utils/inputValidators/loginInputs.js
@@ -1,18 +1,22 @@
 const Joi = require('joi');
 const schema = Joi.object({
   tel: Joi.string()
+    .required()
     .trim()
     .empty()
     .length(11)
     .messages({
+      "any.required": "tel field must be provided.",
       "string.empty": "tel filed can't be empty.",
       "string.length": "tel field must contain 11 characters.",
     }),
 
   password: Joi.string()
+    .required()
     .trim()
     .empty()
     .messages({
+      "any.required": "password field must be provided.",
       "string.empty": "password field can't be empty.",
     }),
 });
--- utils/inputValidators/signupInputs.js
+++ utils/inputValidators/signupInputs.js
@@ -1,17 +1,21 @@
 const Joi = require('joi');
 const schema = Joi.object({
   tel: Joi.string()
+    .required()
     .trim()
     .length(11)
     .messages({
+      "any.required": "tel field must be provided.",
       "string.length": "tel filed must contain 11 characters."
     }),
 
   password: Joi.string()
+    .required()
     .trim()
     .empty()
     .min(6)
     .messages({
+      "any.required": "password must be provided.",
       "string.empty": "password can't be empty.",
       "string.min": "password should contain at least 6 characters.",
     }),
