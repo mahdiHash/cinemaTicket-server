02170d72d41855a2c1f76e0bf56cfd4001f10671
mahdiHash <mahdihashemiii939@gmail.com>
2023-02-11 15:10:57 +0330

    feat(route): add reset password route

--- routers/user.js
+++ routers/user.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const getUserProfile = require('../controllers/user/getUserProfile');
 const updateProfile = require('../controllers/user/updateProfile');
+const resetPass = require('../controllers/user/resetPass');
 
 router.get('/profile', getUserProfile);
 
 router.put('/update', updateProfile);
+
+router.put('/resetPass', resetPass);
 
 module.exports = router;
