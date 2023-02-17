c4f544a9746682cd0ef7b3ad3edd83d1e4a12479
mahdiHash <mahdihashemiii939@gmail.com>
2023-02-17 20:05:09 +0330

    fix(login): decrypt all needed user's info

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -25,11 +25,22 @@ const controller = [
       process.env.JWT_TOKEN_SECRET,
     );
 
-    req.user.tel = decrypt(req.user.tel);
+    // decrypt some vlaues for the client
+    let descryptedUser = {
+      id: req.user.id,
+      full_name: req.user.full_name,
+      tel: decrypt(req.user.tel),
+      email: decrypt(req.user.email),
+      birthday: req.user.birthday,
+      credit_card_num: decrypt(req.user.credit_card_num),
+      national_id: decrypt(req.user.national_id),
+      profile_pic_url: req.user.profile_pic_url,
+    }
+
     res.json({
       token,
-      user: req.user,
-    })
+      user: descryptedUser,
+    });
   }
 ];
 
