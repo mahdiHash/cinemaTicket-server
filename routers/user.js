c34ec2be5225ae215d03f9414de08920208a36ac
mahdiHash <mahdihashemiii939@gmail.com>
2023-02-24 18:55:05 +0330

    feat(route): add user's profile pic remove route

--- routers/user.js
+++ routers/user.js
@@ -2,11 +2,14 @@ const router = require('express').Router();
 const getUserProfile = require('../controllers/user/getUserProfile');
 const updateProfile = require('../controllers/user/updateProfile');
 const resetPass = require('../controllers/user/resetPass');
+const removeProfilePic = require('../controllers/user/removeProfilePic');
 
 router.get('/profile', getUserProfile);
 
 router.put('/update', updateProfile);
 
 router.put('/resetPass', resetPass);
+
+router.delete('/profilePic/remove', removeProfilePic);
 
 module.exports = router;
