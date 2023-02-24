cd5cbf23f7ade45248748ebf568c9b7711c5c5a8
mahdiHash <mahdihashemiii939@gmail.com>
2023-02-24 18:50:52 +0330

    feat(route): add user's profile pic upload route

--- routers/user.js
+++ routers/user.js
@@ -2,11 +2,14 @@ const router = require('express').Router();
 const getUserProfile = require('../controllers/user/getUserProfile');
 const updateProfile = require('../controllers/user/updateProfile');
 const resetPass = require('../controllers/user/resetPass');
+const uploadProfilePic = require('../controllers/user/uploadProfilePic');
 
 router.get('/profile', getUserProfile);
 
 router.put('/update', updateProfile);
 
 router.put('/resetPass', resetPass);
+
+router.put('/profilePic/upload', uploadProfilePic);
 
 module.exports = router;
