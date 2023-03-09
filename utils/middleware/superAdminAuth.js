3e4b09cfee21d5bfc7947c2202b8f59cc0b29e17
mahdiHash <mahdihashemi.code@gmail.com>
2023-03-09 15:32:14 +0330

    fix(auth): super admin auth error message

--- utils/middleware/superAdminAuth.js
+++ utils/middleware/superAdminAuth.js
@@ -1,7 +1,7 @@
 const ForbiddenErr = require('../../utils/errors/forbiddenErr');
 const middleware = (req, res, next) => {
   if (req.user.access_level !== 'super') {
-    return next(new ForbiddenErr('You are not authorized to create admins.'));
+    return next(new ForbiddenErr('Only super admins can access/take this resource/action.'));
   }
 
   next();
