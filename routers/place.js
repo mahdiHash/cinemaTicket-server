2ebc51799f65b5bbd1cb8c19ef509b3bf8ee423c
mahdiHash <mahdihashemi.code@gmail.com>
2023-03-15 14:34:04 +0330

    feat(place): track registeration status

--- routers/place.js
+++ routers/place.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const register = require('../controllers/place/register');
+const trackRegisterStat = require('../controllers/place/trackRegisterStat');
+
+router.get('/registerStat/:code', trackRegisterStat);
 
 router.post('/register', register);
 
