878befaaf1483a7fa26b7c5a0a00a6fc2beddf8f
mahdiHash <mahdihashemi.code@gmail.com>
2023-03-20 02:49:44 +0330

    feat(place): approve place registeration

---
+++ controllers/place/approvePlaceRegister.js
@@ -0,0 +1,53 @@
+const prisma = require('../../config/prismaConfig');
+const passport = require('../../config/passportConfig');
+const superAdminAuth = require('../../utils/middleware/superAdminAuth');
+const bcrypt = require('bcryptjs');
+const NotFoundErr = require('../../utils/errors/notFoundErr');
+const BadRequestErr = require('../../utils/errors/badRequestErr');
+
+const controller = [
+  // authorization
+  passport.authenticate('adminJwt', { session: false }),
+
+  superAdminAuth,
+
+  async (req, res, next) => {
+    if (!isFinite(req.params.id)) {
+      return next(new BadRequestErr('id parameter is not valid.'));
+    }
+
+    let register = await prisma.non_approved_places.findUnique({
+      where: { id: +req.params.id },
+    })
+      .catch(next);
+
+    if (!register) {
+      return next(new NotFoundErr('place register not found.'));
+    }
+
+    if (register.status === 'denied' || register.status === 'approved') {
+      return next(new BadRequestErr('the state of this register can\'t be changed.'));
+    }
+
+    await prisma.places.create({
+      data: {
+        owner_id: register.owner_id,
+        name: register.name,
+        type: register.type,
+        license_id: register.license_id,
+        address: register.address,
+        password: bcrypt.hashSync(register.code),
+      },
+    })
+      .catch(next);
+
+    prisma.non_approved_places.update({
+      where: { id: register.id },
+      data: { status: 'approved' },
+    })
+      .then(() => res.end())
+      .catch(next);
+  }
+];
+
+module.exports = controller;
--- routers/place.js
+++ routers/place.js
@@ -3,6 +3,7 @@ const register = require('../controllers/place/register');
 const trackRegisterStat = require('../controllers/place/trackRegisterStat');
 const cancelRegister = require('../controllers/place/cancelRegister');
 const getAllRegisters = require('../controllers/place/getAllRegisters');
+const approvePlaceRegister = require('../controllers/place/approvePlaceRegister');
 
 router.get('/register/status/:code', trackRegisterStat);
 
@@ -11,5 +12,7 @@ router.delete('/register/cancel/:code', cancelRegister);
 router.get('/register/all', getAllRegisters);
 
 router.post('/register', register);
+
+router.post('/register/approve/:id', approvePlaceRegister);
 
 module.exports = router;
