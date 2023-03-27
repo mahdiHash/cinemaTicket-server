ca6ca6fabcec82acb1fd3913dfb554b3dfbd19de
mahdiHash <mahdihashemi.code@gmail.com>
2023-03-27 13:19:32 +0330

    fix(celeb): create celeb failure wiht no image uploaded

--- controllers/celebrity/create.js
+++ controllers/celebrity/create.js
@@ -21,30 +21,35 @@ const controller = [
   storeValidatedInputs(inputValidator),
 
   async (req, res, next) => {
-    let fileReasStream = fs.createReadStream(req.file.path);
-
+    
     // create celebrity record in db
     prisma.celebrities.create({
       data: res.locals.validBody,
     })
       // upload profile pic
       .then(async (celeb) => {
-        let fileInfo = await imageKit.upload({
-          file: fileReasStream,
-          fileName: `celebPic${celeb.id}`,
-          folder: 'celeb',
-          useUniqueFileName: false, // no unique postfix, so if there's already a pic of the celeb it would be overwritten
-        })
+        let uploadedFileInfo;
+        
+        if (req.file) {
+          let fileReasStream = fs.createReadStream(req.file.path);
+          fileReasStream.on('end', fileReasStream.destroy.bind(fileReasStream));
+          uploadedFileInfo = await imageKit.upload({
+            file: fileReasStream,
+            fileName: `celebPic${celeb.id}`,
+            folder: 'celeb',
+            useUniqueFileName: false, // no unique postfix, so if there's already a pic of the celeb it would be overwritten
+          })
+        }
 
-        return { fileInfo, celeb };
+        return { uploadedFileInfo, celeb };
       })
       // update celebrity record with profile pic url
-      .then(async ({ fileInfo, celeb }) => {
+      .then(async ({ uploadedFileInfo, celeb }) => {
         let upCeleb = await prisma.celebrities.update({
           where: { id: celeb.id },
           data: {
-            profile_pic_fileId: fileInfo.fileId,
-            profile_pic_url: fileInfo.filePath,
+            profile_pic_fileId: uploadedFileInfo?.fileId,
+            profile_pic_url: uploadedFileInfo?.filePath,
           }
         });
 
@@ -52,9 +57,10 @@ const controller = [
       })
       .catch(next)
       .finally(() => {
-        fileReasStream.destroy();
-        fsPromise.rm(req.file.path)
-          .catch(errorLogger.bind(null, { title: 'FILE REMOVEL ERROR' }));
+        if (req.file) {
+          fsPromise.rm(req.file.path)
+            .catch(errorLogger.bind(null, { title: 'FILE REMOVEL ERROR' }));
+        }
       })
   }
 ];
