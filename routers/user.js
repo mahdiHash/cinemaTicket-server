8ae935dd1ce83b9ee850c2da24e57029e165fa98
mahdiHash <mahdihashemi.code@gmail.com>
2023-06-02 00:21:51 +0330

    fix(user): remove user profile pic upload route

--- routers/user.js
+++ routers/user.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const getUserProfile = require('../controllers/user/getUserProfile');
 const updateProfile = require('../controllers/user/updateProfile');
 const resetPass = require('../controllers/user/resetPass');
-const uploadProfilePic = require('../controllers/user/uploadProfilePic');
 const removeProfilePic = require('../controllers/user/removeProfilePic');
 const userLogout = require('../controllers/user/logout');
 
@@ -11,8 +10,6 @@ router.get('/profile', getUserProfile);
 router.put('/update', updateProfile);
 
 router.put('/resetPass', resetPass);
-
-router.put('/profilePic/upload', uploadProfilePic);
 
 router.delete('/profilePic/remove', removeProfilePic);
 
