31737e267ce22ea19288df9a9d3303dba3e48743
mahdiHash <mahdihashemi.code@gmail.com>
2023-06-09 23:31:23 +0330

    fix(apikey): error on apikey absence

--- src/middlewares/authorizeApiKey.ts
+++ src/middlewares/authorizeApiKey.ts
@@ -1,9 +1,14 @@
 import { Request, Response} from 'express';
-import { ApiKeyErr } from '../helpers/errors';
+import { ApiKeyErr, BadRequestErr } from '../helpers/errors';
 import { prisma } from '../config';
 
 async function middleware(req: Request, res: Response) {
   let key = req.headers.apikey as string;
+
+  if (key === null || key === undefined || typeof key !== 'string') {
+    throw new ApiKeyErr();
+  }
+
   let record = await prisma.api_keys.findUnique({ where: { key } });
   
   if (record === null) {
