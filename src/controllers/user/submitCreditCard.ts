dca359277a9f3d4155526ab8c4f94aa24ab32785
mahdiHash <mahdihashemi.code@gmail.com>
2023-06-19 13:37:52 +0330

    fix(user): check for credit card auth req duplicate

--- src/controllers/user/submitCreditCard.ts
+++ src/controllers/user/submitCreditCard.ts
@@ -4,16 +4,30 @@ import { encrypt } from '../../helpers';
 import { prisma, passport } from '../../config';
 import { creditCardInpValidator } from '../../validation/inputValidators';
 import { users } from '@prisma/client';
+import { ForbiddenErr } from '../../helpers/errors';
 
 const controller = [
   passport.authenticate('jwt', { session: false }),
 
   middlewareWrapper(storeValidatedInputs(creditCardInpValidator)),
 
+  middlewareWrapper(checkForDuplicateReq),
+
   middlewareWrapper(middleware),
 ];
 
 export { controller as submitCreditCard };
+
+async function checkForDuplicateReq(req: Request, res: Response) {
+  const reqUserObj = req.user as users;
+  let duplicate = await prisma.credit_card_auth.findFirst({
+    where: { user_id: reqUserObj.id }
+  });
+
+  if (duplicate) {
+    throw new ForbiddenErr('تنها یک درخواست می‌توان در آن واحد ثبت کرد.');
+  }
+}
 
 async function middleware(req: Request, res: Response) {
   const reqUserObj = req.user as users;
