5e7ba5de946aa0b463efd005427d88589aa5eaee
mahdiHash <mahdihashemi.code@gmail.com>
2023-06-20 11:15:24 +0330

    chore(middleware): remove files upon errors

--- src/middlewares/middlewareWrapper.ts
+++ src/middlewares/middlewareWrapper.ts
@@ -5,22 +5,45 @@
 
 import { Request, Response, NextFunction } from 'express';
 import { middlewareWrapperParam } from '../types/types/middlewareWrapperParam';
+import { rm } from 'fs/promises';
+import { errorLogger } from '../helpers/errors';
+import { File } from 'buffer';
 
-function middlewareWrapper(
-  middleware: middlewareWrapperParam,
-) {
+function middlewareWrapper(middleware: middlewareWrapperParam) {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       await middleware(req, res);
 
       if (res.headersSent) {
         res.end();
-      }
-      else {
+      } else {
         next();
       }
-    }
-    catch (err) {
+    } catch (err) {
+      if (req.file) {
+        rm(req.file.path).catch(
+          errorLogger.bind(null, { title: 'FILE REMOVAL ERROR' })
+        );
+      }
+
+      if (req.files) {
+        if (Array.isArray(req.files)) {
+          for (let file of req.files) {
+            rm(file.path).catch(
+              errorLogger.bind(null, { title: 'FILE REMOVAL ERROR' })
+            );
+          }
+        } else {
+          for (let key in req.files) {
+            for (let file of req.files[key]) {
+              rm(file.path).catch(
+                errorLogger.bind(null, { title: 'FILE REMOVAL ERROR' })
+              );
+            }
+          }
+        }
+      }
+
       next(err);
     }
   };
