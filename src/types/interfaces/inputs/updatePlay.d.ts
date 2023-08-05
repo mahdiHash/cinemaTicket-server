47580ecd5f05b95e7173373e4035bd34497cf208
mahdiHash <mahdihashemi.code@gmail.com>
2023-08-05 22:09:21 +0330

    chore(types): update play create and update inputs

--- src/types/interfaces/inputs/updatePlay.d.ts
+++ src/types/interfaces/inputs/updatePlay.d.ts
@@ -9,6 +9,10 @@ interface updatePlay {
   genre?: genresType[];
   type?: playType;
   publish_date?: string;
+  intro?: string;
+  celebrities?: number[];
+  cover_url?: string;
+  cover_fileId?: string;
 }
 
 export { updatePlay as updatePlayInputs };
