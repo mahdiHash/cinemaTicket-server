58c27f1dc491018755ecfa83c17b0cbd5befec66
mahdiHash <mahdihashemi.code@gmail.com>
2023-08-23 10:58:37 +0330

    chore(types): update user profile update interface

--- src/types/interfaces/inputs/userProfileUpdate.d.ts
+++ src/types/interfaces/inputs/userProfileUpdate.d.ts
@@ -3,7 +3,11 @@ interface userProfileUpdateInputs {
   last_name?: string;
   tel?: string;
   email?: string;
+  national_id?: string;
+  credit_card_num?: string;
   birthday?: string;
+  profile_pic_url?: string;
+  profile_pic_fileId?: string;
 }
 
 export { userProfileUpdateInputs }
